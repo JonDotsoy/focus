9be2589c68781a766fdd9db9e534f29be1d2c8fb
Jonathan Delgado <hi@jon.soy>
2024-08-29 14:34:30 -0400

    feat: upgrade makefile

--- formule.rs
+++ formule.rs
@@ -1,10 +1,10 @@
 class Focus < Formula
   desc ""
   homepage "https://github.com/JonDotsoy/focus#readme"
-  url "https://api.github.com/repos/JonDotsoy/cofy/tarball/q-v0.8.0"
-  sha256 "c640c1ca843728d1e9c0515bad166e15de10aded9806ec831f3bd3d969faa609"
+  url "https://api.github.com/repos/JonDotsoy/focus/tarball/de65b86176b923ed19f957d4466d9a8ec0b72b60"
+  sha256 "656d979e82bd9f8bd66b856c5090a1421e14bfd3e45c568272a594a9f5253708"
   license "MIT"
-  version "q-v0.8.0"
+  version "v0.1.0"
 
   depends_on "oven-sh/bun/bun"
   depends_on "make"
@@ -12,7 +12,8 @@ class Focus < Formula
   def install
     system "make", "install"
     system "make", "build"
-    bin.install "dist/cofy" => "cofy"
+    bin.install "bin/focus" => "focus"
+    bin.install "bin/focusd" => "focusd"
   end
 
   test do
