52e195d8920758e53e91961c9c56d0089e7eecbc
Jonathan Delgado <hi@jon.soy>
2024-08-29 22:45:59 -0400

    feat: add utility get

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -6,14 +6,41 @@ export const get = (obj: unknown, ...paths: PropertyKey[]): unknown => {
   return get(Reflect.get(obj, path), ...nextPaths);
 };
 
-get.string = (obj: unknown, ...paths: PropertyKey[]) => {
-  const value = get(obj, ...paths);
-  if (typeof value !== "string") return undefined;
-  return value;
-};
+const types = [
+  "string",
+  "number",
+  "boolean",
+  "function",
+  "bigint",
+  "symbol",
+] as const;
+const createValidatorPrimitiveType =
+  <T>(type: (typeof types)[number]) =>
+  (obj: unknown, ...paths: PropertyKey[]): undefined | T => {
+    const value = get(obj, ...paths);
+    if (typeof value !== type) return undefined;
+    return value as T;
+  };
 
-get.number = (obj: unknown, ...paths: PropertyKey[]) => {
-  const value = get(obj, ...paths);
-  if (typeof value !== "number") return undefined;
-  return value;
-};
+const createValidatorCustomType =
+  <T>(test: (value: unknown) => boolean) =>
+  (obj: unknown, ...paths: PropertyKey[]): undefined | T => {
+    const value = get(obj, ...paths);
+    if (!test(value)) return undefined;
+    return value as T;
+  };
+
+get.string = createValidatorPrimitiveType<string>("string");
+get.number = createValidatorPrimitiveType<number>("number");
+get.boolean = createValidatorPrimitiveType<boolean>("boolean");
+get.function = createValidatorPrimitiveType<Function>("function");
+get.bigint = createValidatorPrimitiveType<bigint>("bigint");
+get.symbol = createValidatorPrimitiveType<symbol>("symbol");
+get.array = createValidatorCustomType<Array<unknown>>((value) =>
+  Array.isArray(value),
+);
+get.date = createValidatorCustomType<Date>((value) => {
+  if (value instanceof Date) return true;
+  if (typeof value === "string") return !isNaN(Date.parse(value));
+  return false;
+});
