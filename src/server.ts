e26288d90e4880e8a8a330cc9ffae796d7101433
Jonathan Delgado <hi@jon.soy>
2024-08-30 22:57:13 -0400

    perf: migrate to rpc service

--- src/server.ts
+++ src/server.ts
@@ -1,61 +1,14 @@
-import { Router, cors } from "artur";
 import { configs } from "./configs.js";
 import { styleText } from "@jondotsoy/style-text";
-import { store, timers } from "./db/store.js";
+import { serviceGenerator as createRouterService } from "./utils/service_generator.js";
 
-const router = new Router({
-  middlewares: [cors()],
-});
-
-router.use("GET", "/timer", {
-  fetch: async () => {
-    return Response.json(await timers.getTimers());
-  },
-});
-
-router.use("GET", "/timer/current", {
-  fetch: async () => {
-    const timer = await timers.currentTime();
-    return Response.json(timer);
-  },
-});
-
-router.use("POST", "/timer/stop", {
-  fetch: async () => {
-    const timer = await timers.stopTimer();
-    return Response.json({});
-  },
-});
-
-router.use("POST", "/timer", {
-  fetch: async (req) => {
-    const url = new URL(req.url);
-    const title = url.searchParams.get("title");
-    if (!title)
-      return new Response(`Missing title on query params`, { status: 400 });
-
-    const timer = await timers.createTimer(title);
-
-    return Response.json(timer, { status: 201 });
-  },
-});
-
-router.use("PUT", "/timer/note", {
-  fetch: async (req) => {
-    const url = new URL(req.url);
-    const note = url.searchParams.get("note") ?? "";
-
-    const timer = await timers.updateNote(note);
-
-    return Response.json(timer, { status: 201 });
-  },
-});
+const serviceRouter = await createRouterService(import("./services.js"));
 
 const server = Bun.serve({
   hostname: configs.server.host,
   port: configs.server.port,
   fetch: async (req) =>
-    (await router.fetch(req)) ?? new Response(null, { status: 404 }),
+    (await serviceRouter.fetch(req)) ?? new Response(null, { status: 404 }),
 });
 
 console.log(`Server ready on ${styleText("green", server.url.toString())}`);
